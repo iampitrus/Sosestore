8e26fd6ef2cb72bff833dd21110e92639674b515
iampitrus <ogbonnapete210@gmail.com>
2024-02-05 21:19:49 +0100

    fix prod

--- Web/src/components/TopBands.tsx
+++ Web/src/components/TopBands.tsx
@@ -1,5 +1,3 @@
-import BrandCard from "./BrandCards";
-
 function TopBands() {
   return (
     <section className="container-x py-6">
@@ -10,31 +8,69 @@ function TopBands() {
         <hr className="border-2 border-blue-500 rounded-full w-[130%]" />
       </div>
       <div className="grid grid-cols-3 gap-4">
-        <BrandCard
-          title={"MamaGold"}
-          logo={"logo"}
-          primary={"gray-900"}
-          secondary={"gray-700"}
-          accent={"gray-800"}
-          image={"./mamagoldrice1.png"}
-          isMamaGold
-        />
-        <BrandCard
-          title={"HoneyWell"}
-          primary={"orange-300"}
-          secondary={"orange-500"}
-          accent={"orange-400"}
-          image={"./honeywellsemolina.jpg"}
-          logo={"./honeywellLogo.jpeg"}
-        />
-        <BrandCard
-          title={"BUA"}
-          primary={"pink-300"}
-          secondary={"pink-500"}
-          accent={"pink-400"}
-          image={"./buaspag2.jpg"}
-          logo={"./bualogo.jpg"}
-        />
+        {/* Mamagold */}
+        <div
+          className={`flex flex-1 overflow-hidden bg-gray-900 p-3 justify-between rounded-2xl relative`}
+        >
+          <div className="flex flex-col gap-2 justify-between">
+            <p className={`text-white w-fit rounded-lg bg-gray-600 px-6 py-2`}>
+              MamaGold
+            </p>
+            <div className="w-20 h-20 bg-white flex items-center justify-center">
+              Logo
+            </div>
+            <p className={`text-white text-2xl uppercase font-semibold`}>
+              up to 10% off
+            </p>
+          </div>
+          <div className=" z-10">
+            <img width={130} src={"./mamagoldrice1.png"} alt="brand" />
+          </div>
+          <div
+            className={`rounded-full absolute w-60 h-60 -top-20 -right-10 bg-gray-600 outline outline-1 outline-offset-4 outline-gray-700`}
+          ></div>
+        </div>
+
+        {/* Honeywell */}
+        <div
+          className={`flex flex-1 overflow-hidden bg-orange-300 p-3 justify-between rounded-2xl relative`}
+        >
+          <div className="flex flex-col gap-2 justify-between">
+            <p className={` w-fit rounded-lg bg-orange-500 px-6 py-2`}>
+              HoneyWell
+            </p>
+            <div className="w-20 h-20 bg-white flex items-center justify-center">
+              <img src={"./honeywellLogo.jpeg"} alt="Logo" />
+            </div>
+            <p className={`text-2xl uppercase font-semibold`}>up to 10% off</p>
+          </div>
+          <div className=" z-10">
+            <img width={130} src={"./honeywellsemolina.jpg"} alt="brand" />
+          </div>
+          <div
+            className={`rounded-full absolute w-60 h-60 -top-20 -right-10 bg-orange-300 outline outline-1 outline-offset-4 outline-orange-400`}
+          ></div>
+        </div>
+        {/* Bua */}
+        <div
+          className={`flex flex-1 overflow-hidden bg-pink-300 p-3 justify-between rounded-2xl relative`}
+        >
+          <div className="flex flex-col gap-2 justify-between">
+            <p className={` w-fit rounded-lg bg-pink-500 px-6 py-2`}>
+              HoneyWell
+            </p>
+            <div className="w-20 h-20 bg-white flex items-center justify-center">
+              <img src={"./bualogo.jpg"} alt="Logo" />
+            </div>
+            <p className={`text-2xl uppercase font-semibold`}>up to 10% off</p>
+          </div>
+          <div className=" z-10">
+            <img width={130} src={"./buaspag2.jpg"} alt="brand" />
+          </div>
+          <div
+            className={`rounded-full absolute w-60 h-60 -top-20 -right-10 bg-pink-500 outline outline-1 outline-offset-4 outline-pink-400`}
+          ></div>
+        </div>
       </div>
       <div className="flex justify-center gap-2 mt-6">
         <div className="w-5 h-2 rounded-full bg-blue-500"></div>
